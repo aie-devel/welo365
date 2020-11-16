b810465475a7d5ca421f3b31ba51ae6273c1ba55
welocalize <aie-devel@welocalize.com>
2020-11-16 06:20:32 -0500

    0.2.0

--- welo365/account.py
+++ welo365/account.py
@@ -70,30 +70,31 @@ class WorkSheet(WorkSheet):
     pass
 
 
-class O365Account:
-    def __init__(self, creds: tuple[str, str] = None, scopes: list[str] = None, scrape: bool = True):
-        self.creds = creds or (os.environ.get('welo365_client_id'), os.environ.get('welo365_client_secret'))
-        self.scopes = scopes or ['offline_access', 'Sites.Manage.All']
-        self.account = Account(self.creds, auth_flow_type='authorization', scopes=scopes)
-        self.con = self.account.con
+class O365Account(Account):
+    def __init__(
+            self,
+            site: str = None,
+            creds: tuple[str, str] = None,
+            scopes: list[str] = None,
+            scrape: bool = False
+    ):
+        creds = creds or (os.environ.get('welo365_client_id'), os.environ.get('welo365_client_secret'))
+        scopes = scopes or ['offline_access', 'Sites.Manage.All']
+        super().__init__(creds=creds, scopes=scopes)
         if scrape:
-            self.con = self.scrape(self.con, self.scopes)
-        if not self.account.is_authenticated:
+            self.scrape(scopes)
+        if not self.is_authenticated:
             self.authenticate()
-
-        self.storage = self.account.storage()
         self.drives = self.storage.get_drives()
-        self.sharepoint = self.account.sharepoint
         self.my_drive = self.storage.get_default_drive()
-        self.site = None
+        self.site = site
         self.root_folder = self.my_drive.get_root_folder()
 
-    @staticmethod
-    def scrape(con: Connection, scopes: list[str]):
+    def scrape(self, scopes: list[str]):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
         driver = webdriver.Chrome(options=chrome_options)
-        auth_url, _ = con.get_authorization_url(requested_scopes=scopes)
+        auth_url, _ = self.con.get_authorization_url(requested_scopes=scopes)
         driver.get(auth_url)
         driver.implicitly_wait(5)
         email = driver.find_element_by_xpath('.//input[@type="email"]')
@@ -110,12 +111,11 @@ class O365Account:
         submit = driver.find_element_by_xpath('.//input[@value="Yes"]')
         submit.click()
         driver.implicitly_wait(3)
-        con.request_token(driver.current_url)
+        self.con.request_token(driver.current_url)
         driver.quit()
-        return con
 
     def authenticate(self):
-        result = self.account.authenticate()
+        result = self.authenticate()
 
     def get_drive(self):
         return self.my_drive
@@ -123,13 +123,12 @@ class O365Account:
     def get_root_folder(self):
         return self.root_folder
 
-    def get_folder_from_path(self, folder_path: str, site: str = None):
+    def get_folder_from_path(self, *subfolders: str):
         folder_path = folder_path[1:] if folder_path[0] == '/' else folder_path
 
         if folder_path is None:
             return self.my_drive
 
-        subfolders = folder_path.split('/')
         if len(subfolders) == 0:
             return self.my_drive
 
