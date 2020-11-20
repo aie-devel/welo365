5927fbb5af2e6b4e43baa59ea029f570bcc9036f
welocalize <aie-devel@welocalize.com>
2020-11-20 16:31:39 -0500

    bump to 1.0.9

--- welo365/excel.py
+++ welo365/excel.py
@@ -10,7 +10,7 @@ from O365.excel import WorkSheet as _WorkSheet
 class Range(_Range):
     pattern = r'^.*!(?P<left>[A-Z]+)(?P<top>[0-9]+)(:(?P<right>[A-Z]+)(?P<bottom>[0-9]+))?$'
 
-    def __init__(self, address: str):
+    def __init__(self, parent):
         super().__init__(address)
         self.matchgroup = re.search(self.pattern, self.address).groupdict()
 
